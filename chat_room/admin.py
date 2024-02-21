33ec01ee2a9edfd8b95d64b5ba904cef521e6413
Praks <prakashsatvi00@gmail.com>
2024-02-21 12:39:33 +0530

    update admin registation for manage database

--- chat_room/admin.py
+++ chat_room/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
+from .models import ChatRoom, Message
 
-# Register your models here.
-from .models import ChatRoom
+class MessageInline(admin.TabularInline):
+    model = Message
+    extra = 0
 
-admin.site.register(ChatRoom)
+class ChatRoomAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    inlines = [MessageInline]
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('chatroom', 'user', 'content', 'date_added')
+    list_filter = ('chatroom', 'user')
+
+admin.site.register(ChatRoom, ChatRoomAdmin)
+admin.site.register(Message, MessageAdmin)
